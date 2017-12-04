58095554673bcf325173a349a2ec87acc896d4bf
Sienna Mori <siennamori@Macintosh.local>
2017-12-04 10:42:02 -0500

    prob 1 commit

---
+++ Sienna/R_scripts/Homework-11-Mine-/configuration.R
@@ -0,0 +1,6 @@
+library(dplyr)
+library(ggmap)
+
+source("../../R_scripts/Homework-11-Mine-/data.R")
+source("../../R_scripts/Homework-11-Mine-/Tile.R")
+source("../../R_scripts/Homework-11-Mine-/ManyTile.R")
