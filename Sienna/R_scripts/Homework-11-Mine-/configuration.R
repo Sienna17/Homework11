0ce17c6262f7883295397dc504d4fcf8b99ebb68
Sienna17 <33759180+Sienna17@users.noreply.github.com>
2017-12-04 18:51:01 -0500

    Delete configuration.R

--- Sienna/R_scripts/Homework-11-Mine-/configuration.R
+++
@@ -1,6 +0,0 @@
-library(dplyr)
-library(ggmap)
-
-source("../../R_scripts/Homework-11-Mine-/data.R")
-source("../../R_scripts/Homework-11-Mine-/Tile.R")
-source("../../R_scripts/Homework-11-Mine-/ManyTile.R")
